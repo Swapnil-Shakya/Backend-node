8ff97975cf827e2c974a550d1039c89fff300528
Swapnil Shakya <swapnil.shakya99@gmail.com>
2025-08-28 22:48:20 +0530

    Util code discussion

--- src/index.js
+++ src/index.js
@@ -8,7 +8,18 @@ dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (err) => {
+        console.log("Error connecting to database", err)
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!", err)});   
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statusCode, data, messgage = "successs") {
+        this.statusCode = statusCode,
+        this.data = data,
+        this.message = message
+        this.success = statusCode < 400
+    }
+} 
