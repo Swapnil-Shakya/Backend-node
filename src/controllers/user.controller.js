0f9138d59f52eb77ee8cfd3c09d26b03d1f68bc0
Swapnil Shakya <swapnil.shakya99@gmail.com>
2025-09-04 18:56:58 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 
+
 const registerUser = asyncHandler (async (req, res) => {
     
     // get user details from frontend
@@ -18,8 +19,10 @@ const registerUser = asyncHandler (async (req, res) => {
     // return response  
 
 
-    const { fullName, email, username, password} = req.body
-    console.log("email", email);
+    const { fullName, email, username, password } = req.body
+    // console.log("email", email);
+    // console.log("req.body:", req.body);
+    // console.log("req.files:", req.files);
     
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -27,7 +30,7 @@ const registerUser = asyncHandler (async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -36,7 +39,12 @@ const registerUser = asyncHandler (async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
